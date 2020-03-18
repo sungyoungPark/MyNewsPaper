6191b9e54eeaf1a663d22faf35a86d8287aa311b
sungyoungPark <sygitter1491012@gmail.com>
2020-03-18 20:10:33 +0900

    Initial Commit

---
+++ MyNewsPaper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNewsPaper
+//
+//  Created by 박성영 on 18/03/2020.
+//  Copyright © 2020 박성영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
